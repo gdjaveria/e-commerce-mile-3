09bd7251856f663a4d1d18ac2cf1a7c15c4028ce
gdjaveria <javeriajiapk@gmail.com>
2025-02-03 16:02:17 -0800

    committ

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -23,7 +23,7 @@ const ProductDetails = () => {
   const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
-    if (!id) return; // Ensure `id` exists before fetching
+    if (!id) return; 
     const fetchProduct = async () => {
       const response = await fetch(`https://fakestoreapi.com/products/${id}`);
       const data = await response.json();
